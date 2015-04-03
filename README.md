ccbaae13810a4fb1fdc1609715226e26d476ffa6
The Floods <thefloods@Flood-MacBook.attlocal.net>
2015-04-03 16:37:19 -0400

    Final Submission

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+Welcome to Verchema's Implementation of Frogger!
 
-for self-checking their submission.
+Objective:
+Your goal is to reach the water without hitting any of the enemy bugs!
+
+Scoring:
+-  You'll earn 100 points every time you reach the water successfully.
+-  You'll lose 100 points and a life every time you collide with a bug.
+-  You'll earn a bonus 500 points AND one life after every 500 points.
+
+How to Play:
+- Use your arrow keys to move the Players left, right, up, or down avoiding any of the enemy bugs.
+
+Good luck!
--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function(x,y,speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
+    
+    //Track corners of the enemy positions for use in collision detection
     this.top = this.y;
     this.right = this.x + 70;
     this.left = this.x;
@@ -28,9 +30,7 @@ Enemy.prototype.update = function(dt) {
     else
     {
         this.x = 0;
-    }
-    
-
+    } 
 }
 
 // Draw the enemy on the screen, required method for game
@@ -41,41 +41,59 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function (x,y,lives) {
+var Player = function (x,y,lives, score) {
 
     this.x = x;
     this.y = y;
+    //Track corners of the player positions for use in collision detection
     this.top = this.y;
     this.right = this.x + 70;
     this.left = this.x;
     this.bottom = this.y + 70;
     this.lives = lives;
+    this.score = score;
     this.sprite = 'images/char-horn-girl.png';
-  
+    console.log("Starting Score", this.score, "Starting Lives",this.lives);
 }
 
 Player.prototype.update = function() {
-    
+    if (player.lives == 0) {
+        console.log("Game Over!");
+        console.log("Final Score", this.score);
+        this.lives = 3;
+        this.score = 0;
+        player.reset();
+    }
 }
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-   
-   
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
 }
 
-// Move player back to beginning if he reaches the water
+// Move player back to beginning if he reaches the water or collides with enemy
 Player.prototype.reset = function() {
-    
     this.x = 200;
     this.y = 400;
+    console.log("Current Score", player.score, "Current Lives",player.lives);
+}
 
+Player.prototype.addtoscore = function() {
+    this.score += 100;
+}
+
+Player.prototype.bonus = function() {
+    if (this.score >499) {
+        console.log ("Congratulations!  You've earned 500 points and an extra life!");
+        this.score += 500;
+        this.lives += 1;
+    }
+    
 }
 
 // Implement logic based on key pressed
 Player.prototype.handleInput = function(direction) {
-
+    //Check if selected movement will move players off the board
     if (direction == 'right' && this.x < 400) {
         this.x = this.x + 100;
     }
@@ -88,7 +106,11 @@ Player.prototype.handleInput = function(direction) {
     if (direction == 'down' && this.y < 400) {
         this.y = this.y + 100;
     } 
+    // If player reaches the water
     if (this.y == 0)   {
+        player.addtoscore();
+        //Check if player eligible for bonus then reset
+        player.bonus();
         player.reset();
     }
 }
@@ -99,10 +121,9 @@ function checkCollisions () {
             enemy.x + 70 > player.x &&
             enemy.y < player.y + 50 &&
             enemy.y + 70 > player.y) 
-        {
-            console.log('collision');
+        {   //If player collides with enemy
             player.lives --;
-            console.log(player.lives);
+            player.score -= 100;
             player.reset();
         }
     });
@@ -115,10 +136,7 @@ var enemy1 = new Enemy(0,0,200);
 var enemy2 = new Enemy(100,100, 150);
 var enemy3 = new Enemy(200,200, 125);
 var allEnemies = [enemy1, enemy2, enemy3];
-var player = new Player(200, 400, 3);
-
-
-
+var player = new Player(200, 400, 3, 0);
 
 
 // This listens for key presses and sends the keys to your
@@ -131,7 +149,6 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
     
-    player.handleInput(allowedKeys[e.keyCode]);
-    
+    player.handleInput(allowedKeys[e.keyCode]);    
 
 });
